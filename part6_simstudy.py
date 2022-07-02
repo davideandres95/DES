690c27abda1b9f69122dd35223b67e7d3e37facb
David de Andrés Hernandez <davideandres95@gmail.com>
2022-07-02 09:54:15 +0200

    completed part 6

--- part6_simstudy.py
+++ part6_simstudy.py
@@ -1,5 +1,9 @@
+from matplotlib import pyplot
+
+from counter import TimeIndependentCounter
+from simulation import Simulation
 from statistictests import ChiSquare
-import numpy
+import numpy as np
 
 """
 This file should be used to keep all necessary code that is used for the verification section in part 6
@@ -13,9 +17,72 @@ def task_6_2_1():
     First, 100 samples are drawn from a normal distribution. Afterwards the chi square test is run on them to see,
     whether they follow the original or another given distribution.
     """
-    # TODO Task 6.2.1: Your code goes here
-    pass
+    alpha = .1
+    values = []
+    np.random.seed(0)
+    for _ in range(100):
+        values.append(np.random.normal(0, 1))
+
+    emp_n, emp_x = np.histogram(values, bins=100, range=(-5, 5))
+
+    cs = ChiSquare(emp_n=emp_n, emp_x=emp_x)
+
+    [c1, c2] = cs.test_distribution(alpha, 0, 1)
+
+    print(f'chi_square table:{c2}')
+    print(f'chi_square calculated:{c1}')
+
+    if (c2 > c1):
+        print('The H0 has not been rejected')
+    else:
+        print('The H0 has been rejected')
+
+    return c2 > c1
+
+
+def task_6_3_2():
+    sim = Simulation()
+    sim.sim_param.S = 5
+    sim.sim_param.RHO = .01
+    sim.sim_param.SIM_TIME = 100000
+
+    cnt_sys_util = TimeIndependentCounter(name='sys_util')
+
+    for _ in range(100):
+        sim.reset()
+        cnt_sys_util.count(sim.do_simulation().system_utilization)
+
+    min_val = np.array(cnt_sys_util.values).min()
+    max_val = np.array(cnt_sys_util.values).max()
+
+    histogram, bins = np.histogram(cnt_sys_util.values, bins=20, range=(min_val, max_val))
+
+    # weights = np.full(len(cnt_sys_util.values), 1.0 / float(len(cnt_sys_util.values)))
+    pyplot.hist(bins[:-1], bins, alpha=0.5, rwidth=.7, weights=histogram, label='Sys. Util.')
+    pyplot.title(f"ρ={sim.sim_param.RHO}")
+    pyplot.ylabel("P(x)")
+    pyplot.xlabel("x")
+    pyplot.legend(loc='upper left')
+    pyplot.xlim([0, 0.025])
+    pyplot.show()
+    cnt_sys_util.report()
+    print('\n')
+
+    cs = ChiSquare(emp_n=histogram, emp_x=bins)
+
+    [c1, c2] = cs.test_distribution(0.1, cnt_sys_util.get_mean(), cnt_sys_util.get_var())
+
+    print(f'chi_square table:{c2}')
+    print(f'chi_square calculated:{c1}')
+
+    if (c2 > c1):
+        print('The H0 has not been rejected')
+    else:
+        print('The H0 has been rejected')
+
+    return c2 > c1
 
 
 if __name__ == '__main__':
     task_6_2_1()
+    task_6_3_2()
--- statistictests.py
+++ statistictests.py
@@ -1,3 +1,13 @@
+import math
+
+import numpy as np
+from scipy.stats import chi2, norm, chisquare
+
+
+def pdf_gaussian(x, mean, var):
+    return (1 / np.sqrt(2 * np.pi * var)) * np.exp(-0.5 * np.square(x - mean) / var)
+
+
 class ChiSquare(object):
 
     def __init__(self, emp_x, emp_n, name="default"):
@@ -7,9 +17,13 @@ class ChiSquare(object):
         :param emp_n: frequency
         :param name: name for better distinction of tests
         """
-        self.name = name
-        # TODO Task 6.1.1: Your code goes here
-        pass
+        if len(emp_x) - 1 == len(emp_n):
+            self.name = name
+            self.emp_x = list(emp_x)
+            self.emp_n = list(emp_n)
+        else:
+            raise ValueError("Wrong array lengths. emp_x must have k+1 values representing the bins' borders."
+                             "emp_n with k values represent the frequencies of each bin")
 
     def test_distribution(self, alpha, mean, var):
         """
@@ -18,5 +32,62 @@ class ChiSquare(object):
         :param mean: mean value of the gaussian distribution
         :param var: variance of the gaussian distribution
         """
-        # TODO Task 6.1.1: Your code goes here
-        pass
+
+        n = sum(self.emp_n)
+        cdf_values = norm.cdf(np.array(self.emp_x), loc=mean, scale=np.sqrt(var))
+        expected_freq = []
+        for j in range(len(cdf_values) - 1):
+            expected_freq.append(cdf_values[j + 1] - cdf_values[j])
+        expected_freq = list(np.array(expected_freq) * n)
+        # assert sum(expected_freq)==n
+
+        k = 0
+        head_freq = 0
+        while head_freq < 5:
+            head_freq += expected_freq[k]
+            k += 1
+
+        head_freq = 0
+        head_obs = 0
+        for _ in range(k):
+            head_freq += expected_freq[_]
+            head_obs += self.emp_n[_]
+
+        for _ in range(k - 1):
+            expected_freq.pop(0)
+            self.emp_n.pop(0)
+
+        expected_freq[0] = head_freq
+        self.emp_n[0] = head_obs
+
+        tail_freq = 0
+        k = len(expected_freq) - 1
+        while tail_freq < 5:
+            tail_freq += expected_freq[k]
+            k -= 1
+
+        tail_freq = 0
+        tail_obs = 0
+        for _ in range(k + 1, len(expected_freq) - 1):
+            tail_freq += expected_freq[_]
+            tail_obs += self.emp_n[_]
+
+        for _ in range(k + 1, len(expected_freq) - 1):
+            expected_freq.pop()
+            self.emp_n.pop()
+
+        expected_freq[len(expected_freq) - 1] = tail_freq
+        self.emp_n[len(self.emp_n) - 1] = tail_obs
+
+        print(f'O_i = {self.emp_n}')
+        print(f'E_i = {expected_freq}')
+
+        df = len(self.emp_n) - 2 - 1
+        # find Chi-Square value from table
+        chi_square = chi2.ppf(1 - alpha, df=df)
+
+        chi_hat = 0
+        for obs, expect in zip(self.emp_n, expected_freq):
+            chi_hat += ((obs - expect) ** 2) / expect
+
+        return chi_hat, chi_square
